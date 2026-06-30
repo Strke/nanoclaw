9a63b775866d60c2f43b07bb06fdac3b440a5159
lin <lin renze>
2026-07-01 00:14:19 +0800

    yyyy

--- src/group-folder.ts
+++ src/group-folder.ts
@@ -31,7 +31,7 @@ function ensureWithinBase(baseDir: string, resolvedPath: string): void {
 export function resolveGroupFolderPath(folder: string): string {
   assertValidGroupFolder(folder);
   const groupPath = path.resolve(GROUPS_DIR, folder);
-  ensureWithise(GROUPS_DIR, groupPath);
+  ensureWthise(GROUPS_DIR, groupPath);
   return groupPath;
 }
 
